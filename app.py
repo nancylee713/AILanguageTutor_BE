5664181a22cd71dca7960f4b12da2a8bc5d8dde5
Scott <swschipke@gmail.com>
2019-12-29 15:22:36 -0700

    Log users in and sign them up

--- app.py
+++ app.py
@@ -34,10 +34,28 @@ def create_new_user():
     print("Email : {}, Password: {}".format(req['email'],req['password']))
     hash = bcrypt.generate_password_hash(req['password']).decode('utf-8')
     print(hash)
-#     user = User(email=req['email'], password=hash)
+    check_user_exists = User.query.filter_by(email=req['email']).first()
+    if check_user_exists:
+            return jsonify({'error': 'Email already exists'}), 409
+
+
+    user = User(email=req['email'], password=hash)
 #     db.session.add(user)
 #     db.session.commit()
     return jsonify(req), 222
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login_user():
+        req=request.get_json()
+        user=User.query.filter_by(email=req['email']).first()
+        print(user)
+        # print(bcrypt.check_password_hash(user.password, req['password']))
+        if user and bcrypt.check_password_hash(req['password'], user.password):
+                profile=UserProfile.query.filter_by(user_id=user.id).first()
+                print(profile)
+        return jsonify('testing')
+
 
 
 @app.route('/users')
