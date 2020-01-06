780d1a1cdaee234f6612edc85e6726574033524c
nancylee713 <srlee713@gmail.com>
2020-01-06 15:34:18 -0700

    Add unit tests
    Co-authored-by: Scott Schipke <swschipke@gmail.com>

--- app.py
+++ app.py
@@ -21,24 +21,8 @@ unsplash_access_key = app.config['UNSPLASH_ACCESS_KEY']
 
 
 @app.route("/")
-def generate_speech_question():
-    word_list = ['cup', 'dish', 'fork']
-    level = 'beginner'
-
-    for text in word_list:
-        params = { "query": text }
-        headers = {
-            "Accept-Version": "v1",
-            "Authorization": "Client-ID {}".format(unsplash_access_key)
-        }
-        response = requests.get("https://api.unsplash.com/search/photos", params=params, headers=headers)
-        image_url = json.loads(response.text)["results"][0]["urls"]["full"]
-
-        speech_question = SpeechQuestion(text=text, level=level, image_url=image_url)
-        db.session.add(speech_question)
-        db.session.commit()
-
-    print("{} {}-level words have been added to the database!".format(len(word_list), level))
+def hello():
+    return "Hello World!"
 
 @app.route("/signup", methods=['GET','POST'])
 def create_new_user():
