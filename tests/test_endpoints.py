cf2333f26d30e6b0068abb86ab360353186d3554
nancylee713 <srlee713@gmail.com>
2020-01-06 19:37:07 -0700

    Comment out user-related tests
    Co-authored-by: Scott Schipke <swschipke@gmail.com>

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -23,56 +23,6 @@ class TestFlaskApi(unittest.TestCase):
         self.app = app.app.test_client()
         self.app.testing = True
 
-    def test_get_all_users(self):
-        response = self.app.get(USERS_URL)
-        data = json.loads(response.get_data())
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(data), 7)
-
-
-    def test_get_one_user(self):
-        response = self.app.get(USERS_URL)
-        data = json.loads(response.get_data())
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data[0]['email'], 'test@email.com')
-
-    def test_get_all_users_profile(self):
-        response = self.app.get(USER_PROFILE_URL)
-        data = json.loads(response.get_data())
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(data), 3)
-
-    def test_get_one_user_profile(self):
-        response = self.app.get(USER_PROFILE_URL)
-        data = json.loads(response.get_data())
-        keys = ['age', 'created_date', 'id', 'name', 'proficiency', 'updated_date', 'user_id']
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(list(data[0]), keys)
-
-    def test_post_user_already_exists(self):
-        # duplicate attribute = bad
-        user = {"email": "test@email.com", "password": "password"}
-        response = self.app.post(SIGNUP_URL,
-                                 data=json.dumps(user),
-                                 content_type='application/json')
-        self.assertEqual(response.status_code, 409)
-
-    def test_post_new_user(self):
-        user = {"email": "test101@email.com", "password": "password"}
-        response = self.app.post(SIGNUP_URL,
-                                 data=json.dumps(user),
-                                 content_type='application/json')
-        self.assertEqual(response.status_code, 222)
-
-    # need to include test for missing value field
-
-    # def test_get_new_user_profile(self):
-    #     user = {"email": "test35@email.com", "password": "password"}
-    #     response = self.app.get(SIGNUP_URL,
-    #                              data=json.dumps(user),
-    #                              content_type='application/json')
-    #     self.assertEqual(response.status_code, 222)
-
     def test_get_all_speech_questions(self):
         response = self.app.get(SPEECH_QUESTIONS_URL)
         data = json.loads(response.get_data())
@@ -84,6 +34,56 @@ class TestFlaskApi(unittest.TestCase):
         data = json.loads(response.get_data())
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(data), 15)
+
+    # def test_get_all_users(self):
+    #     response = self.app.get(USERS_URL)
+    #     data = json.loads(response.get_data())
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(len(data), 7)
+    #
+    #
+    # def test_get_one_user(self):
+    #     response = self.app.get(USERS_URL)
+    #     data = json.loads(response.get_data())
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(data[0]['email'], 'test@email.com')
+    #
+    # def test_get_all_users_profile(self):
+    #     response = self.app.get(USER_PROFILE_URL)
+    #     data = json.loads(response.get_data())
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(len(data), 3)
+    #
+    # def test_get_one_user_profile(self):
+    #     response = self.app.get(USER_PROFILE_URL)
+    #     data = json.loads(response.get_data())
+    #     keys = ['age', 'created_date', 'id', 'name', 'proficiency', 'updated_date', 'user_id']
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(list(data[0]), keys)
+    #
+    # def test_post_user_already_exists(self):
+    #     # duplicate attribute = bad
+    #     user = {"email": "test@email.com", "password": "password"}
+    #     response = self.app.post(SIGNUP_URL,
+    #                              data=json.dumps(user),
+    #                              content_type='application/json')
+    #     self.assertEqual(response.status_code, 409)
+    #
+    # def test_post_new_user(self):
+    #     user = {"email": "test101@email.com", "password": "password"}
+    #     response = self.app.post(SIGNUP_URL,
+    #                              data=json.dumps(user),
+    #                              content_type='application/json')
+    #     self.assertEqual(response.status_code, 222)
+
+    # need to include test for missing value field
+
+    # def test_get_new_user_profile(self):
+    #     user = {"email": "test35@email.com", "password": "password"}
+    #     response = self.app.get(SIGNUP_URL,
+    #                              data=json.dumps(user),
+    #                              content_type='application/json')
+    #     self.assertEqual(response.status_code, 222)
 
 if __name__ == "__main__":
     unittest.main()
